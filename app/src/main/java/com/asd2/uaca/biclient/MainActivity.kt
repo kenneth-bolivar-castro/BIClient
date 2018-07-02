85dc837da894bdee2cf2578a0a8a4305e8d04f8f
Kenneth Bolivar Castro <keboca@gmail.com>
2018-07-01 22:58:06 -0600

    Show up Toast

--- app/src/main/java/com/asd2/uaca/biclient/MainActivity.kt
+++ app/src/main/java/com/asd2/uaca/biclient/MainActivity.kt
@@ -2,11 +2,20 @@ package com.asd2.uaca.biclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnTabMe = findViewById<Button>(R.id.btnTabMe)
+        btnTabMe.setOnClickListener{
+            val editText = findViewById<EditText>(R.id.editText)
+            Toast.makeText(this, editText.text.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
