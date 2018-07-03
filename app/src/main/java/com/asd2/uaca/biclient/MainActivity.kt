921be78b2ebbfe06b864e3f1c7679d07fa864f31
Kenneth Bolivar Castro <keboca@gmail.com>
2018-07-03 00:17:51 -0600

    Added settings activity

--- app/src/main/java/com/asd2/uaca/biclient/MainActivity.kt
+++ app/src/main/java/com/asd2/uaca/biclient/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.asd2.uaca.biclient
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log.v
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -17,5 +22,21 @@ class MainActivity : AppCompatActivity() {
             val editText = findViewById<EditText>(R.id.editText)
             Toast.makeText(this, editText.text.toString(), Toast.LENGTH_LONG).show()
         }
+
+        v("MainActivity",  SettingsActivity.API_TOKEN_TEXT)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.item_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
